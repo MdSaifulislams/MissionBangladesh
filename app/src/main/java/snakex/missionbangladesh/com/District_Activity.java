4ab7e4ed28cb588eecd276b2627e5ac74be32ccc
oliul-hasnat-rafi <oliulhasnatrafi270@gmail.com>
2023-01-16 23:14:33 +0600

    District name setup

--- app/src/main/java/snakex/missionbangladesh/com/District_Activity.java
+++ app/src/main/java/snakex/missionbangladesh/com/District_Activity.java
@@ -7,30 +7,20 @@ import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.util.ArrayList;
+
 public class District_Activity extends AppCompatActivity {
     ListView simpleList;
-    String countryList[] = {"ঢাকা",
-            "নরসিংদী",
-            "ফরিদপুর",
-            "গোপালগঞ্জ",
-            "মাদারীপুর",
-            "শরীয়তপুর",
-            "রাজবাড়ী" ,
-            "গাজীপুর" ,
-            "মানিকগঞ্জ" ,
-            "মুন্সীগঞ্জ" ,
-            "নারায়নগঞ্জ" ,
-            "টাঙ্গাইল" ,
-            "কিশোরগঞ্জ" };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_district);
-
         simpleList = (ListView)findViewById(R.id.simpleListView);
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, R.layout.activity_listview, R.id.textView, countryList);
+        ArrayList<String> district_name = (ArrayList<String>) getIntent().getSerializableExtra("key");
+        String Div_name = getIntent().getStringExtra("divname");
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, R.layout.activity_listview, R.id.textView, district_name);
         simpleList.setAdapter(arrayAdapter);
-       getSupportActionBar().setTitle("ঢাকা বিভাগ");
+       getSupportActionBar().setTitle(Div_name+" বিভাগ");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
--- app/src/main/java/snakex/missionbangladesh/com/Division_Adapter.java
+++ app/src/main/java/snakex/missionbangladesh/com/Division_Adapter.java
@@ -10,12 +10,13 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Division_Adapter extends RecyclerView.Adapter<Division_Adapter.Divisionviewholder> {
         Context context;
         List<Division> divisionList;
-
+    ArrayList<String> district_name;
     public Division_Adapter(Context context, List<Division> divisionList) {
         this.context = context;
         this.divisionList=divisionList;
@@ -39,15 +40,56 @@ public class Division_Adapter extends RecyclerView.Adapter<Division_Adapter.Divi
     public void onBindViewHolder(@NonNull Divisionviewholder holder, int position) {
         Division division = this.divisionList.get(position);
         holder.Division_name.setText(division.getName());
-        holder.itemView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent =new Intent(context,District_Activity.class);
-                context.startActivity(intent);
+
+        holder.itemView.setOnClickListener(view -> {
+            switch (position) {
+                case 0:
+                    district_name = new ArrayList<>();
+                    district_name.add("ঢাকা");
+                    district_name.add("নরসিংদী");
+                    district_name.add("ফরিদপুর");
+                    district_name.add("গোপালগঞ্জ");
+                    district_name.add("মাদারীপুর");
+                    district_name.add("শরীয়তপুর");
+                    district_name.add("রাজবাড়ী");
+                    district_name.add("গাজীপুর");
+                    district_name.add("মানিকগঞ্জ");
+                    district_name.add("মুন্সীগঞ্জ");
+                    district_name.add("নারায়নগঞ্জ");
+                    district_name.add("টাঙ্গাইল");
+                    district_name.add("কিশোরগঞ্জ");
+                    Intent intent = new Intent(context, District_Activity.class);
+                    intent.putExtra("key", district_name);
+                    intent.putExtra("divname",division.getName());
+                    context.startActivity(intent);
+                    break;
+             //   চট্টগ্রাম, কুমিল্লা, কক্সবাজার, নোয়াখালী, ব্রাহ্মণবাড়িয়া, চাঁদপুর, লক্ষ্মীপুর, ফেনী, খাগড়াছড়ি, রাঙ্গামাটি, বান্দরবান
+                    case 1:
+                     district_name = new ArrayList<>();
+                    district_name.add("চট্টগ্রাম");
+                    district_name.add("কুমিল্লা");
+                    district_name.add("কক্সবাজার");
+                    district_name.add("নোয়াখালী");
+                    district_name.add("ব্রাহ্মণবাড়িয়া");
+                    district_name.add("চাঁদপুর");
+                    district_name.add("লক্ষ্মীপুর");
+                    district_name.add("ফেনী");
+                    district_name.add("খাগড়াছড়ি");
+                    district_name.add("রাঙ্গামাটি");
+                    district_name.add("বান্দরবান");
+                    Intent intent1 = new Intent(context, District_Activity.class);
+                    intent1.putExtra("key", district_name);
+                    intent1.putExtra("divname",division.getName());
+                    context.startActivity(intent1);
+                    break;
+
+
             }
         });
-
     }
+
+
+
 
    public class Divisionviewholder extends RecyclerView.ViewHolder{
         TextView Division_name;
